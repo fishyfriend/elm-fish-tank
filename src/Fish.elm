6373056d67fdc45ff31af5947ecd20d20377becd
fishyfriend <fishyfriend@users.noreply.github.com>
2015-10-12 00:14:23 -0400

    tails!

--- src/Fish.elm
+++ src/Fish.elm
@@ -2,7 +2,7 @@ module Fish where
 
 
 import Color exposing (..)
-import Graphics.Collage exposing (..)
+import Graphics.Collage as C
 
 import Physics
 import Point
@@ -11,8 +11,15 @@ import Point
 type alias Fish = Physics.Object {}
 
 
-render : Fish -> Form
-render {pos}
-  = oval 40 20
-  |> filled green
-  |> move (Point.toPair pos)
+render : Fish -> C.Form
+render {pos,vel} =
+  let
+    body = C.oval 80 40
+    tail = C.polygon
+      [ (-40, -25)
+      , (-40, 25)
+      , (-20, 0) ]
+    forms = List.map (C.filled green) [body, tail]
+    group = C.group forms
+  in
+     group |> C.move (Point.toPair pos)
